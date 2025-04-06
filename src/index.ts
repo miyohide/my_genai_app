45c96496b7214cf6e3007052f39aa69ff6a4c3f7
Hidenori Miyoshi <miyohide@gmail.com>
2025-04-06 10:16:58 +0900

    create bedrock chat app.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
+import { BedrockChat } from '@langchain/community/chat_models/bedrock';
 
-console.log('Hello from TypeScript CLI!');
+export const main = async (): Promise<void> => {
+    const model = new BedrockChat({
+        region: 'ap-northeast-1',
+        model: 'anthropic.claude-v2:1',
+    });
+
+    const res = await model.invoke([
+        ['human', 'LangChainとはなんですか']
+    ]);
+    console.log(JSON.stringify(res, null, 2));
+};
+
+main();
